cbef5bf0ab88833a50ac64078fe164b41bf88521
HarithaSudheesh <haritha601974@gmail.com>
2024-08-26 22:21:12 +0530

    employers model

--- job_portal/app/admin.py
+++ job_portal/app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from . models import Candidate, JobCategories, Language, Qualification
+from . models import Candidate, JobCategories, Language, Qualification,Employer
 
 # Register your models here.
 
@@ -7,5 +7,6 @@ admin.site.register(Candidate)
 admin.site.register(JobCategories)
 admin.site.register(Language)
 admin.site.register(Qualification)
+admin.site.register(Employer)
 
 
