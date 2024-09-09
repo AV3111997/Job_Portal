d4ba3cd360e76d2e6d8caa430aea88c55d96038b
rayif700 <141204368+rayif007@users.noreply.github.com>
2024-09-09 22:20:17 +0530

    location with null value

--- job_portal/app/models.py
+++ job_portal/app/models.py
@@ -26,12 +26,12 @@ class JobCategory(models.Model):
     def __str__(self):
         return self.name
 
-
 class Location(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
         return self.name
+
 
 
 class Candidate(models.Model):
