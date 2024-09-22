9f4f6f98bbc0008f68b432b43860393e9c604a50
sredha prasad <sredhaprasad123@gmail.com>
2024-09-22 21:23:24 +0530

    candidate_list and delete added

--- job_portal/administrator/views.py
+++ job_portal/administrator/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from app.models import Employer
+from app.models import Employer,Candidate
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView 
 from django.views.generic.edit import DeleteView
@@ -19,3 +19,13 @@ class EmployerDeleteView(DeleteView):
     success_url = reverse_lazy('employer_list')
 
 
+
+class CandidateListView(ListView):
+    model = Candidate
+    template_name = 'candidates/list.html' 
+    context_object_name = 'candidatelist'  
+
+class CandidateDeleteView(DeleteView):
+    model = Candidate
+    template_name = 'employers/delete.html'
+    success_url = reverse_lazy('employer_list')
